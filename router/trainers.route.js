8b76573a3e7c30c2e2f6c9015d03554a09ca0487
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-27 15:07:20 +0800

    add update trainer but encrypt hash not working

--- router/trainers.route.js
+++ router/trainers.route.js
@@ -3,6 +3,17 @@ const db = require("../db/models/index.js");
 
 const router = express.Router();
 
+//Get all
+router.get("/", async (req, res, next) => {
+  try {
+    const trainers = await db.Trainer.findAll();
+
+    res.status(200).json(trainers);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // Add POST /trainers route
 router.post("/", async (req, res, next) => {
   try {
@@ -13,5 +24,37 @@ router.post("/", async (req, res, next) => {
     next(err);
   }
 });
+
+//Update method
+// router.post("/update/:username", async (req, res, next) => {
+//   try {
+//     const username = req.params.username;
+//     const data = req.body;
+//     const trainerToUpdate = await db.Trainer.findAll({
+//       where: { username: username },
+//     });
+//     console.log(trainerToUpdate);
+//     if (trainerToUpdate === null) res.sendStatus(404);
+//     else {
+//       //   const [count, updateObject] = await db.Trainer.update(data, {
+//       //     returning: true,
+//       //     raw: true,
+//       //     where: { username: username },
+//       //   });
+//       const [count, updateObject] = await db.Trainer.update(
+//         { password: data.password },
+//         {
+//           returning: true,
+//           raw: true,
+//           where: { username: username },
+//         }
+//       );
+
+//       res.status(200).send(updateObject);
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 module.exports = router;
