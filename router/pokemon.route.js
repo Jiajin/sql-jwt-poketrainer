ad99b98586d33ae9cf18473a5b73e18eb77764de
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-27 11:31:09 +0800

    add getById and Post

--- router/pokemon.route.js
+++ router/pokemon.route.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const { findAllPokemon } = require("../crud/read");
+const db = require("../db/models/index.js");
+const { Op } = require("sequelize");
 
 router.get("/", async (req, res, next) => {
   try {
-    const pokemons = await findAllPokemon();
+    const pokemons = await db.Pokemon.findAll();
 
-    res.json(pokemons);
+    res.status(200).json(pokemons);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:id", async (req, res, next) => {
+  try {
+    const pokemons = await db.Pokemon.findByPk(req.params.id);
+
+    res.status(200).json(pokemons);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const created = await db.Pokemon.create(req.body);
+
+    // The recommended way to log an instance, but do note that this might still log sensitive data stored in database. Need processing.
+    console.log(created.toJSON());
+
+    res.status(201).json(created.toJSON());
   } catch (error) {
     next(error);
   }
