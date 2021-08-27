6ef626d7a6f797a93f10bf6b21c9ab4910390e04
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-27 14:41:52 +0800

    add trainer with password encrypt hook

--- db/models/trainer.js
+++ db/models/trainer.js
@@ -1,5 +1,6 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class Trainer extends Model {
     /**
@@ -19,6 +20,19 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "Trainer",
+      hooks: {
+        beforeCreate: async (user) => {
+          const salt = await bcrypt.genSaltSync(10); // this generates the salt
+          user.password = bcrypt.hashSync(user.password, salt); // this sets the hashed value as the password
+        },
+        beforeUpdate: async (user) => {
+          // add beforeUpdate hook here
+          if (user.password) {
+            const salt = await bcrypt.genSaltSync(10);
+            user.password = bcrypt.hashSync(user.password, salt);
+          }
+        },
+      },
     }
   );
   return Trainer;
