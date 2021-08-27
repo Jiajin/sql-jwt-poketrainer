00f3dcebdb77aa196d78225b30ba8110df887968
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-27 10:31:03 +0800

    update sequelizerc and add pokemon model and migration model

---
+++ db/models/pokemon.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Pokemon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pokemon.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        // validate: {
+        //   notEmpty: true,
+        // },
+        allowNull: false,
+        unique: true,
+      },
+      japaneseName: {
+        type: DataTypes.STRING,
+      },
+      baseHp: {
+        type: DataTypes.INTEGER,
+      },
+      category: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Pokemon",
+    }
+  );
+  return Pokemon;
+};
